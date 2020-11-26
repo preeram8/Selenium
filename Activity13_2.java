9f5696b4129dfd36b516bb90f59cfd23a442409b
preeram8 <73516579+preeram8@users.noreply.github.com>
2020-11-26 19:25:15 +0530

    Add files via upload

---
+++ Activity13_2.java
@@ -0,0 +1,34 @@
+package Selenium_Session1;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+public class Activity13_2 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        //Creating the JavascriptExecutor interface object by Type casting
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        
+        //Open browser
+        driver.get("https://www.training-support.net/selenium/lazy-loading");
+        
+        //Find the second card
+        WebElement secondCard = driver.findElement(By.xpath("//div[@class='spaced'][2]//div[@class='image']/img"));
+        
+        //Before Scrolling
+        System.out.println("Image src: " + secondCard.getAttribute("src"));
+        
+        //Scroll the card into view
+        js.executeScript("window.scrollBy(0,1000)");
+        
+        wait.until(ExpectedConditions.not(ExpectedConditions.attributeContains(secondCard, "src", "loading")));
+        
+        //After scrolling
+        System.out.println("Image src: " + secondCard.getAttribute("src"));
+        
+        //Close browser
+        driver.close();
+
+    }
+}
